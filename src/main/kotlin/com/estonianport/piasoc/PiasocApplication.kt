6657d083e68c9825b84194779a6b083add0a7669
RdzSebastian <rdzsebastian@gmail.com>
2023-10-20 11:19:46 -0300

    Ajustes de estructura de proyecto

--- src/main/kotlin/com/estonianport/piasoc/PiasocApplication.kt
+++ src/main/kotlin/com/estonianport/piasoc/PiasocApplication.kt
@@ -1,7 +1,9 @@
 package com.estonianport.piasoc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 class PiasocApplication
@@ -9,3 +11,12 @@ class PiasocApplication
 fun main(args: Array<String>) {
 	runApplication<PiasocApplication>(*args)
 }
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(PiasocApplication::class.java)
+	}
+
+}
+
